2f6f26540d5e8a0d7b39682cfbbc998dedbab693
sebDtSci <sebastientadiello@gmail.com>
2024-06-21 17:50:41 +0200

    bug fixed

--- resize_images.py
+++ resize_images.py
@@ -3,15 +3,15 @@ import os
 
 def resize_image(input_path, output_path, size):
     with Image.open(input_path) as img:
-        img = img.resize(size, Image.ANTIALIAS)
+        img = img.resize(size, Image.LANCZOS)
         img.save(output_path)
 
 def resize_images_in_directory(directory, size):
     for filename in os.listdir(directory):
-        if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endwith(".webp") or filename.endwith(".jfif"):
+        if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".webp") or filename.endswith(".jfif"):
             input_path = os.path.join(directory, filename)
             output_path = os.path.join(directory, filename)
             resize_image(input_path, output_path, size)
             print(f"Resized {filename} to {size}")
 
-resize_images_in_directory("docs/images", (150, 150))
+resize_images_in_directory("C:\\Users\\stadiello\\Documents\\blog\\docs\\images", (150, 150))
